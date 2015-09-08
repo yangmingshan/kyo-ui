3f5e01ad1662225512823a9c425cb4a2e6f0bc2b
kkdashu <wmeng17@gmail.com>
2015-09-08 22:01:08 +0800

    项目说明

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # kyo-ui
+提供基础ui组件
+
+## 安装
+
+* `git clone` this repository
+* `npm install`
+* `bower install`
+
+## demo
+
+* `gulp`
+* 浏览http://localhost:9200
+
+## 构建
+
+* `webpack`
--- dist/kyo-ui.js
+++ dist/kyo-ui.js
@@ -56,15 +56,15 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	var Component = __webpack_require__(1);
 	var DatePicker = __webpack_require__(2);
-	var AutoComplete = __webpack_require__(5);
-	var SwitchTab = __webpack_require__(7);
-	var Mask = __webpack_require__(18);
-	var Dialog = __webpack_require__(20);
-	var Confirm = __webpack_require__(23);
-	var Alert = __webpack_require__(24);
-	var Loading = __webpack_require__(25);
+	var AutoComplete = __webpack_require__(8);
+	var SwitchTab = __webpack_require__(11);
+	var Mask = __webpack_require__(23);
+	var Dialog = __webpack_require__(26);
+	var Confirm = __webpack_require__(31);
+	var Alert = __webpack_require__(32);
+	var Loading = __webpack_require__(33);
 
-	var AutoParse = __webpack_require__(26);
+	var AutoParse = __webpack_require__(34);
 
 	//以下控件整个程序应该只有一个
 
@@ -234,7 +234,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(3);
-	__webpack_require__(4);
+	__webpack_require__(7);
 	var Component = __webpack_require__(1);
 
 	var DatePicker = Component.extend({
@@ -286,7 +286,10 @@ return /******/ (function(modules) { // webpackBootstrap
 	// removed by extract-text-webpack-plugin
 
 /***/ },
-/* 4 */
+/* 4 */,
+/* 5 */,
+/* 6 */,
+/* 7 */
 /***/ function(module, exports) {
 
 	/* Chinese initialisation for the jQuery UI date picker plugin. */
@@ -313,12 +316,12 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 5 */
+/* 8 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var AutoComplete, Component;
 
-	__webpack_require__(6);
+	__webpack_require__(9);
 
 	Component = __webpack_require__(1);
 
@@ -362,22 +365,23 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 6 */
+/* 9 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
 
 /***/ },
-/* 7 */
+/* 10 */,
+/* 11 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var Component, SwitchTab, template;
 
-	__webpack_require__(8);
+	__webpack_require__(12);
 
 	Component = __webpack_require__(1);
 
-	template = __webpack_require__(9);
+	template = __webpack_require__(14);
 
 	SwitchTab = Component.extend({
 	  $el: "<div class='switch_tab'></div>",
@@ -421,16 +425,17 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 8 */
+/* 12 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
 
 /***/ },
-/* 9 */
+/* 13 */,
+/* 14 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var Handlebars = __webpack_require__(10);
+	var Handlebars = __webpack_require__(15);
 	module.exports = (Handlebars["default"] || Handlebars).template({"1":function(depth0,helpers,partials,data) {
 	    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
@@ -468,16 +473,16 @@ return /******/ (function(modules) { // webpackBootstrap
 	},"useData":true});
 
 /***/ },
-/* 10 */
+/* 15 */
 /***/ function(module, exports, __webpack_require__) {
 
 	// Create a simple path alias to allow browserify to resolve
 	// the runtime on a supported path.
-	module.exports = __webpack_require__(11)['default'];
+	module.exports = __webpack_require__(16)['default'];
 
 
 /***/ },
-/* 11 */
+/* 16 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -486,30 +491,30 @@ return /******/ (function(modules) { // webpackBootstrap
 
 	exports.__esModule = true;
 
-	var _import = __webpack_require__(12);
+	var _import = __webpack_require__(17);
 
 	var base = _interopRequireWildcard(_import);
 
 	// Each of these augment the Handlebars object. No need to setup here.
 	// (This is done to easily share code between commonjs and browse envs)
 
-	var _SafeString = __webpack_require__(15);
+	var _SafeString = __webpack_require__(20);
 
 	var _SafeString2 = _interopRequireWildcard(_SafeString);
 
-	var _Exception = __webpack_require__(14);
+	var _Exception = __webpack_require__(19);
 
 	var _Exception2 = _interopRequireWildcard(_Exception);
 
-	var _import2 = __webpack_require__(13);
+	var _import2 = __webpack_require__(18);
 
 	var Utils = _interopRequireWildcard(_import2);
 
-	var _import3 = __webpack_require__(16);
+	var _import3 = __webpack_require__(21);
 
 	var runtime = _interopRequireWildcard(_import3);
 
-	var _noConflict = __webpack_require__(17);
+	var _noConflict = __webpack_require__(22);
 
 	var _noConflict2 = _interopRequireWildcard(_noConflict);
 
@@ -542,7 +547,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = exports['default'];
 
 /***/ },
-/* 12 */
+/* 17 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -553,11 +558,11 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.HandlebarsEnvironment = HandlebarsEnvironment;
 	exports.createFrame = createFrame;
 
-	var _import = __webpack_require__(13);
+	var _import = __webpack_require__(18);
 
 	var Utils = _interopRequireWildcard(_import);
 
-	var _Exception = __webpack_require__(14);
+	var _Exception = __webpack_require__(19);
 
 	var _Exception2 = _interopRequireWildcard(_Exception);
 
@@ -820,7 +825,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	/* [args, ]options */
 
 /***/ },
-/* 13 */
+/* 18 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -939,7 +944,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 /***/ },
-/* 14 */
+/* 19 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -982,7 +987,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = exports['default'];
 
 /***/ },
-/* 15 */
+/* 20 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -1001,7 +1006,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	module.exports = exports['default'];
 
 /***/ },
-/* 16 */
+/* 21 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -1019,15 +1024,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	exports.invokePartial = invokePartial;
 	exports.noop = noop;
 
-	var _import = __webpack_require__(13);
+	var _import = __webpack_require__(18);
 
 	var Utils = _interopRequireWildcard(_import);
 
-	var _Exception = __webpack_require__(14);
+	var _Exception = __webpack_require__(19);
 
 	var _Exception2 = _interopRequireWildcard(_Exception);
 
-	var _COMPILER_REVISION$REVISION_CHANGES$createFrame = __webpack_require__(12);
+	var _COMPILER_REVISION$REVISION_CHANGES$createFrame = __webpack_require__(17);
 
 	function checkRevision(compilerInfo) {
 	  var compilerRevision = compilerInfo && compilerInfo[0] || 1,
@@ -1238,7 +1243,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 /***/ },
-/* 17 */
+/* 22 */
 /***/ function(module, exports) {
 
 	/* WEBPACK VAR INJECTION */(function(global) {'use strict';
@@ -1262,12 +1267,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
 
 /***/ },
-/* 18 */
+/* 23 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var Component, Mask;
 
-	__webpack_require__(19);
+	__webpack_require__(24);
 
 	Component = __webpack_require__(1);
 
@@ -1279,22 +1284,23 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 19 */
+/* 24 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
 
 /***/ },
-/* 20 */
+/* 25 */,
+/* 26 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var Component, Dialog, template;
 
-	__webpack_require__(21);
+	__webpack_require__(27);
 
 	Component = __webpack_require__(1);
 
-	template = __webpack_require__(22);
+	template = __webpack_require__(30);
 
 	Dialog = Component.extend({
 	  name: 'dialog',
@@ -1370,16 +1376,18 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 21 */
+/* 27 */
 /***/ function(module, exports) {
 
 	// removed by extract-text-webpack-plugin
 
 /***/ },
-/* 22 */
+/* 28 */,
+/* 29 */,
+/* 30 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var Handlebars = __webpack_require__(10);
+	var Handlebars = __webpack_require__(15);
 	module.exports = (Handlebars["default"] || Handlebars).template({"1":function(depth0,helpers,partials,data) {
 	    return "  <a class=\"kui-dialog-close\">x</a>\n";
 	},"3":function(depth0,helpers,partials,data) {
@@ -1417,12 +1425,12 @@ return /******/ (function(modules) { // webpackBootstrap
 	},"useData":true});
 
 /***/ },
-/* 23 */
+/* 31 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var Confirm, Dialog;
 
-	Dialog = __webpack_require__(20);
+	Dialog = __webpack_require__(26);
 
 	Confirm = Dialog.extend({
 	  name: 'confirm',
@@ -1462,12 +1470,12 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 24 */
+/* 32 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var Alert, Dialog;
 
-	Dialog = __webpack_require__(20);
+	Dialog = __webpack_require__(26);
 
 	Alert = Dialog.extend({
 	  name: 'alert',
@@ -1490,12 +1498,12 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 25 */
+/* 33 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var Dialog, Loading;
 
-	Dialog = __webpack_require__(20);
+	Dialog = __webpack_require__(26);
 
 	Loading = Dialog.extend({
 	  name: 'loading',
@@ -1524,10 +1532,10 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 26 */
+/* 34 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var DatePickerAutoParse = __webpack_require__(27);
+	var DatePickerAutoParse = __webpack_require__(35);
 
 	function AutoParse(id) {
 	    this.id = id;
@@ -1553,7 +1561,7 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 /***/ },
-/* 27 */
+/* 35 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var DatePicker = __webpack_require__(2);
